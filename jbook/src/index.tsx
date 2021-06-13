c83d8ab3655f193aa1ae743accd5d2a0971ff402
Heang Ly <heanglypa@gmail.com>
2021-06-13 12:48:32 -0400

    wire up redux

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -2,13 +2,15 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom'
 // import CodeCell from './components/code-cell'
 import TextEditor from './components/text-editor'
+import { Provider } from 'react-redux'
+import { store } from './state'
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       {/* <CodeCell /> */}
       <TextEditor />
-    </div>
+    </Provider>
   )
 }
 
